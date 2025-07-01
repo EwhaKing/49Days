abc64ff1c0e3b8a120429eb5e63356f8b392aab6
Ssomewhat <isc10120@gmail.com>
2025-07-02 02:00:04 +0900

    feat: TeaIngredient 및 관련 Enum

---
+++ Assets/TeaHouse/Kitchen/Scripts/Enums.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum IngredientName
+{
+    Hub,
+    Rose,
+    Honey
+}
+
+public enum IngredientType
+{
+    TeaLeaf,     // 찻잎
+    Flower,      // 꽃
+    Substitute,  // 대용차재료
+    Additional   // 추가 재료
+}
+
+public enum SpriteStatus
+{
+    Default,     // 기본 상태
+    Chopped,     // 손질된 상태(대용차, 꽃차)
+    Rolled,      // 유념된 상태
+    Mashed,      // 뭉개진(유념실패) 상태
+    Roasted      // 덖은 상태
+}
+
+public enum OxidizedDegree  
+{
+    None,        // 산화 없음 = 0
+    Half,        // 반산화 = 50
+    Full,        // 완전 산화 = 100
+    Over         // 탐
+}
+
+public enum ResultStatus
+{
+    None,        // X
+    Success,     // 성공
+    Failed       // 실패
+}
+
+public enum ProcessStep
+{
+    Chop,             // 손질
+    Oxidize,          // 산화
+    Roast,            // 덖기
+    Roll              // 유념
+}
+
