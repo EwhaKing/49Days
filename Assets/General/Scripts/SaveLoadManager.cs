41939421a3c90f7f95c0e7e54d01490937fe85e9
Ssomewhat <isc10120@gmail.com>
2025-08-09 14:02:48 +0900

    update: SaveLoadManager 수정 - 에디터에서 저장 시 에셋 폴더 하위에 저장됨

--- Assets/General/Scripts/SaveLoadManager.cs
+++ Assets/General/Scripts/SaveLoadManager.cs
@@ -10,33 +10,51 @@ public class SaveLoadManager : SceneSingleton<SaveLoadManager>
     public Action onSave;
     public Action onLoad;
     int date;
+    string saveDirectory;
 
     public void Save<T>(T dataClass)
     {
-        string directory = Application.persistentDataPath + "/" + date;
-        if (!Directory.Exists(directory))
-            Directory.CreateDirectory(directory);
         string json = JsonConvert.SerializeObject(dataClass, Formatting.Indented);
-        File.WriteAllText(directory + "/" + typeof(T).Name, json);
+        File.WriteAllText($"{saveDirectory}/{typeof(T).Name}", json);
     }
 
     public T Load<T>()
     {
-        string json = File.ReadAllText($"{Application.persistentDataPath}/{date}/{typeof(T).Name}");
+        string json = File.ReadAllText($"{saveDirectory}/{typeof(T).Name}");
         return JsonConvert.DeserializeObject<T>(json);
     }
 
     public void SaveAllByDate(int date)
     {
         this.date = date;
+        saveDirectory = GetSaveDirectory();
+        if (!Directory.Exists(saveDirectory))
+        {
+            Directory.CreateDirectory(saveDirectory);
+        }
+        
         onSave?.Invoke();
-        Debug.Log($"데이터 저장됨: {Application.persistentDataPath}/{date}");
+        Debug.Log($"데이터 저장됨: {saveDirectory}");
     }
 
     public void LoadAllByDate(int date)
     {
         this.date = date;
+        saveDirectory = GetSaveDirectory();
+
         onLoad?.Invoke();
-        Debug.Log($"데이터 로드됨: {Application.persistentDataPath}/{date}");
+        Debug.Log($"데이터 로드됨: {saveDirectory}");
     }
+
+    private string GetSaveDirectory()
+    {
+    #if UNITY_EDITOR
+        // 프로젝트 폴더 밑에 저장 (Assets 폴더와 같은 레벨)
+        return Path.Combine(Application.dataPath, "SaveData", date.ToString());
+    #else
+        // 빌드 환경에서는 persistentDataPath
+        return Path.Combine(Application.persistentDataPath, date.ToString());
+    #endif
+    }
+
 }
