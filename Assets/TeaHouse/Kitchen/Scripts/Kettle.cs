01b7589c98d5e9d799d9516a30912fbb462988ab
jigi <micekj@naver.com>
2025-08-24 16:47:28 +0900

    fix : 주전자 판정 널널하게 조절(유저 편의성 고려)

--- Assets/TeaHouse/Kitchen/Scripts/Kettle.cs
+++ Assets/TeaHouse/Kitchen/Scripts/Kettle.cs
@@ -15,7 +15,7 @@ public class Kettle : MonoBehaviour, IPointerEnterHandler, IDragHandler, IPointe
     public float Temperature { get; private set; } = 100;
 
     [SerializeField] float tempChangePerSec = 2f;
-    [SerializeField] float pourRadius = 1f;
+    [SerializeField] float pourRadius = 4f;
 
     //고리에 걸기 위한 거리 판단
     [SerializeField] float hookSnapDistance = 1.2f;
